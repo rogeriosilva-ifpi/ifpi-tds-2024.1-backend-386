11e4a0053ba36a63033290d09949d316aaae50da
Rogerio Silva <rogerio.silva@ifpi.edu.br>
2024-02-20 20:53:49 -0300

    Relacionamentos, Admin, Inline...

--- djangoanimal/catalogo/admin.py
+++ djangoanimal/catalogo/admin.py
@@ -1,12 +1,25 @@
 from django.contrib import admin
 
-from catalogo.models import Produto
+from catalogo.models import Produto, Categoria, Tag
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+  list_display = ['nome']
+  search_fields = ['nome']
+
+
+class TagInlineAdmin(admin.TabularInline):
+  model = Tag
+  extra = 0
 
 
 class ProdutoAdmin(admin.ModelAdmin):
-  list_display = ['nome', 'detalhes', 'preco', 'disponivel']
+  list_display = ['nome', 'detalhes', 'preco', 'categoria', 'disponivel']
   list_filter = ('disponivel',)
   search_fields = ('nome', 'detalhes')
+
+  autocomplete_fields = ['categoria']
+  inlines = [TagInlineAdmin]
 
 
 admin.site.register(Produto, ProdutoAdmin)
--- djangoanimal/catalogo/models.py
+++ djangoanimal/catalogo/models.py
@@ -1,8 +1,27 @@
 from django.db import models
 
+class Categoria(models.Model):
+  nome = models.CharField(max_length=120)
+
+  def __str__(self):
+    return self.nome
+
+
 class Produto(models.Model):
   nome = models.CharField(max_length=120, null=False, 
         blank=False, verbose_name='Descrição')
   detalhes = models.CharField(max_length=1000, blank=True, null=True)
   preco = models.DecimalField(max_digits=6, decimal_places=2)
   disponivel = models.BooleanField(default=False)
+
+  # relacionamentos
+  categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, related_name='produtos', null=True)
+
+
+class Tag(models.Model):
+  nome = models.CharField(max_length=32)
+
+  # relacionamentos
+  produto = models.ForeignKey(Produto, on_delete=models.CASCADE, related_name='tags')
+
+  
